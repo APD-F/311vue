3b5a7aa27c66f89194a12335013decfdbcf0e5fd
ansonsow <60858599+ansonsow@users.noreply.github.com>
2020-02-13 15:47:38 -0800

    personal note

--- src/components/PersonalNotes.vue
+++ src/components/PersonalNotes.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="personal-notes">
+        
         <img src="https://support.apple.com/library/content/dam/edam/applecare/images/de_DE/iOS/ios10-iphone6-notes-icloud-create-checklist.jpg" height="400px">
     </div>
 </template>
